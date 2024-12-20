hvalfangst/azure-blob-triggered-event-grid-published-to-service-bus

Switched from Azure CLI to Bicep for provisioning infrastructure resources

infra/main.bicep
@@ -1,0 +1,81 @@
+targetScope = 'resourceGroup'
+
+@description('The location where resources will be deployed.')
+param location string = 'westeurope'
+
+@description('The name of the storage account.')
+param storageAccountName string = 'hvalfangststorageaccount'
+
+@description('The name of the storage container.')
+param storageContainerName string = 'hvalfangstcontainer'
+
+@description('The name of the App Service Plan.')
+param servicePlanName string = 'hvalfangstserviceplan'
+
+@description('The name of the Application Insights instance.')
+param appInsightsName string = 'hvalfangstapplicationinsights'
+
+@description('The name of the Linux Function App.')
+param functionAppName string = 'hvalfangstlinuxfunctionapp'
+
+resource storageAccount 'Microsoft.Storage/storageAccounts@2022-09-01' = {
+  name: storageAccountName
+  location: location
+  sku: {
+    name: 'Standard_LRS'
+  }
+  kind: 'StorageV2'
+}
+
+resource blobService 'Microsoft.Storage/storageAccounts/blobServices@2022-09-01' = {
+  parent: storageAccount
+  name: 'default'
+}
+
+resource storageContainer 'Microsoft.Storage/storageAccounts/blobServices/containers@2022-09-01' = {
+  parent: blobService
+  name: storageContainerName
+}
+
+resource appServicePlan 'Microsoft.Web/serverfarms@2022-09-01' = {
+  name: servicePlanName
+  location: location
+  sku: {
+    name: 'Y1'
+  }
+  kind: 'Linux'
+}
+
+resource appInsights 'Microsoft.Insights/components@2020-02-02' = {
+  name: appInsightsName
+  location: location
+  kind: 'other'
+  properties: {
+    Application_Type: 'other'
+  }
+}
+
+resource functionApp 'Microsoft.Web/sites@2022-09-01' = {
+  name: functionAppName
+  location: location
+  kind: 'functionapp,linux'
+  properties: {
+    serverFarmId: appServicePlan.id
+    siteConfig: {
+      appSettings: [
+        {
+          name: 'APPINSIGHTS_INSTRUMENTATIONKEY'
+          value: appInsights.properties.InstrumentationKey
+        }
+        {
+          name: 'AzureWebJobsFeatureFlags'
+          value: 'EnableWorkerIndexing'
+        }
+      ]
+      linux_fx_version: 'Python|3.10'
+    }
+  }
+  identity: {
+    type: 'SystemAssigned'
+  }
+}

hvalfangst/azure-static-react-website-triggering-functions

infra/main.bicep
@@ -1,0 +1,101 @@
+targetScope = 'resourceGroup'
+
+@description('The location where resources will be deployed.')
+param location string = 'westeurope'
+
+@description('The name of the storage account.')
+param storageAccountName string = 'hvalfangststorageaccount'
+
+@description('The name of the storage container.')
+param storageContainerName string = 'hvalfangstcontainer'
+
+@description('The name of the App Service Plan.')
+param servicePlanName string = 'hvalfangstserviceplan'
+
+@description('The name of the Application Insights instance.')
+param appInsightsName string = 'hvalfangstapplicationinsights'
+
+@description('The name of the Linux Function App.')
+param functionAppName string = 'hvalfangstlinuxfunctionapp'
+
+resource storageAccount 'Microsoft.Storage/storageAccounts@2022-09-01' = {
+  name: storageAccountName
+  location: location
+  sku: {
+    name: 'Standard_LRS'
+  }
+  kind: 'StorageV2'
+}
+
+resource blobService 'Microsoft.Storage/storageAccounts/blobServices@2022-09-01' = {
+  parent: storageAccount
+  name: 'default'
+}
+
+resource storageContainer 'Microsoft.Storage/storageAccounts/blobServices/containers@2022-09-01' = {
+  parent: blobService
+  name: storageContainerName
+}
+
+resource appServicePlan 'Microsoft.Web/serverfarms@2022-09-01' = {
+  name: servicePlanName
+  location: location
+  properties: {
+    reserved: true // Required for Linux Function Apps
+   }
+  sku: {
+    name: 'Y1'
+  }
+  kind: 'linux'
+}
+
+resource appInsights 'Microsoft.Insights/components@2020-02-02' = {
+  name: appInsightsName
+  location: location
+  kind: 'other'
+  properties: {
+    Application_Type: 'other'
+  }
+}
+
+resource functionApp 'Microsoft.Web/sites@2022-09-01' = {
+  name: functionAppName
+  location: location
+  kind: 'functionapp,linux'
+  identity: {
+    type: 'SystemAssigned'
+  }
+  properties: {
+    serverFarmId: appServicePlan.id
+    siteConfig: {
+      linuxFxVersion: 'Python|3.11'
+      appSettings: [
+        {
+          name: 'AzureWebJobsStorage'
+          value: 'DefaultEndpointsProtocol=https;AccountName=${storageAccountName};EndpointSuffix=${environment().suffixes.storage};AccountKey=${storageAccount.listKeys().keys[0].value}'
+        }
+        {
+          name: 'FUNCTIONS_EXTENSION_VERSION'
+          value: '~4'
+        }
+        {
+          name: 'APPINSIGHTS_INSTRUMENTATIONKEY'
+          value: appInsights.properties.InstrumentationKey
+        }
+        {
+          name: 'FUNCTIONS_WORKER_RUNTIME'
+          value: 'python'
+        }
+        {
+          name: 'SCM_DO_BUILD_DURING_DEPLOYMENT'
+          value: 'true'
+        }
+        {
+          name: 'ENABLE_ORYX_BUILD'
+          value: 'true'
+        }
+      ]
+    }
+    httpsOnly: true
+  }
+}